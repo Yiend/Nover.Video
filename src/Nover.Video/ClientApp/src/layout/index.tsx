0f7f2a9e96415a76c30d92bc5f59c985a4a9e4a1
yangcf02 <yangcf02@mingyuanyun.com>
2023-12-20 20:44:27 +0800

    .

--- src/Nover.Video/ClientApp/src/layout/index.tsx
+++ src/Nover.Video/ClientApp/src/layout/index.tsx
@@ -10,7 +10,7 @@ export default function Layout() {
             <Header/>
             <div className="border-t">
                 <div className="bg-background">
-                    <div className="grid lg:grid-cols-5">
+                    <div className="grid lg:grid-cols-10">
                         <Sidebar className="hidden lg:block" />
                         <div className="col-span-3 lg:col-span-4 lg:border-l">
                             <div className="h-full px-4 py-6 lg:px-8">
